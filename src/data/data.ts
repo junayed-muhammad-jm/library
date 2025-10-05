8e3b9e966e53bf741de4a1c75c9ecd6795773501
junayed-muhammad-jm <junayedmuhammad.jm@gmail.com>
2025-10-05 14:54:23 +0600

    my update

---
+++ src/data/data.ts
@@ -0,0 +1,82 @@
+export interface ImageData {
+  id: number;
+  image: string;
+  name: string;
+  writer: string;
+  src: string;
+}
+
+const ImageData: ImageData[] = [
+  {
+    id: 1,
+    image: "public/i-1.jpg",
+    writer: "মাওলানা মহিউদ্দীন খান ",
+    name: "জীবনের খেলা ঘরে",
+    src: "https://www.rokomari.com/book/44455/jiboner-khalagore",
+  },
+  {
+    id: 2,
+    image: "public/i-2.jpg",
+    writer: "মাহবুব উল্লাহ",
+    name: "আমার জীবন আমার সংগ্রাম। ",
+    src: "https://www.rokomari.com/book/357006/my-life-my-struggle",
+  },
+  {
+    id: 3,
+    image: "public/i-3.jpg",
+    writer: "জন গডেন",
+    name: "বাংলার রোদে আমরা দু’বোন।",
+    src: "https://www.rokomari.com/book/484938/banglar-rode-amra-du-bon",
+  },
+  {
+    id: 4,
+    image: "public/i-4.jpg",
+    writer: "মীর মোশারফ হোসেন ",
+    name: "আমার জীবনি। ",
+    src: "https://www.rokomari.com/book/387081/amer-jiboni",
+  },
+  {
+    id: 5,
+    image: "public/i-5.jpg",
+    writer: "ড এনামুক হক",
+    name: "কারুময় আত্মার ঝাঁপিতে  ",
+    src: "https://www.rokomari.com/book/237937/karumoy-atmar-jhanpite",
+  },
+  {
+    id: 6,
+    image: "public/i-6.jpg",
+    writer: "ড মুহাম্মদ ইব্রাহিম ",
+    name: "জীবনস্মৃতিতে মানুষ দেশ বিজ্ঞান । ",
+    src: "https://www.rokomari.com/book/110732/jibon-smrititee-manush-desh-biggan-1st-khondo",
+  },
+  {
+    id: 7,
+    image: "public/i-7.jpg",
+    writer: "ড মুহাম্মদ ইউনুস ",
+    name: "গ্রামীণ ব্যাংক ও আমার জীবন । ",
+    src: "https://www.rokomari.com/book/75672/grameen-bank-o-amar-jibon",
+  },
+  {
+    id: 8,
+    image: "public/i-8.jpg",
+    writer: "আবুল হায়াত ",
+    name: "জীবন খাতার ফুটনোট।  ",
+    src: "https://www.rokomari.com/book/155863/jibon-khatar-futnot",
+  },
+  {
+    id: 9,
+    image: "public/i-9.jpg",
+    writer: "বিভূতিভূষণ বন্দ্যোপাধ্যায়",
+    name: "স্মৃতির রেখা।  ",
+    src: "https://www.rokomari.com/book/338638/smritir-rekha",
+  },
+  {
+    id: 10,
+    image: "public/i-10.jpg",
+    writer: " সুফিয়া খাতুন",
+    name: "জীবন নদীর বাঁকে বাঁকে।  ",
+    src: "https://www.rokomari.com/book/231310/jibon-nodir-bake-bake",
+  },
+];
+
+export default ImageData;
